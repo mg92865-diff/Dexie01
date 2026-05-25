4894be92da52c0fc1882580e4b65f7481fc91b5e
mg92865-diff <mg92865@gmail.com>
2026-05-25 05:48:17 -0700

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,27 @@
+﻿const CACHE_NAME = 'records-cache-v2';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png',
+  'https://unpkg.com/dexie@3.2.4/dist/dexie.js'
+];
+
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
+    })
+  );
+});
